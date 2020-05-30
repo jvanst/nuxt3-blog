211e21dcaedcdf3a93d5767944441c5e9bab164c
James Vansteenkiste <james@jamesvan.ca>
2020-05-30 14:29:32 -0400

    Add portfolio and more section

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -15,9 +15,9 @@
 
 <section class="py-10">
   <div class="text-center">
-    <h1 class="text-4xl font-medium">About</h1>
+    <h1 class="text-3xl font-medium">About</h1>
   </div>
-  <div class="container mx-auto max-w-3xl px-4 my-10">
+  <div class="container mx-auto max-w-3xl px-4 my-8">
     <p class="pb-3">
       I am a full stack developer located in London, Ontario. I have 3 years experience with Vue.js, Node.js and designing RESTful applications. 
       I have worked on projects using mySQL and mongoDB databases, and have become efficient developing with effective, efficient database structures. 
@@ -30,10 +30,10 @@
     <div class="flex flex-wrap mt-3">
       <div class="w-full sm:w-1/2 text-center">
         <img src="./profile.jpeg" class="w-9/12 mx-auto my-8 rounded" alt="">
-        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <a href="./resume.pdf" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center" download>
           <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
           <span>Download Resume</span>
-        </button>
+        </a>
       </div>
       <div class="w-full sm:w-1/2">
         <ul class="mt-6 mx-5">
@@ -43,7 +43,7 @@
                 <img src="./icons/vuejs.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">Vue.js, Svelte & React</h4>
+                <div class="text-lg">Vue.js, Svelte & React</div>
                 <span class="text-sm text-gray-700">Frontend Technology</span>
               </div>
             </div>
@@ -54,7 +54,7 @@
                 <img src="./icons/nodejs.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">NodeJs & PHP</h4>
+                <div class="text-lg">NodeJs & PHP</div>
                 <span class="text-sm text-gray-700">Backend Technology</span>
               </div>
             </div>
@@ -65,7 +65,7 @@
                 <img src="./icons/api.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">RESTful JSON</h4>
+                <div class="text-lg">RESTful JSON</div>
                 <span class="text-sm text-gray-700">API Design</span>
               </div>
             </div>
@@ -76,7 +76,7 @@
                 <img src="./icons/database.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">MySQL</h4>
+                <div class="text-lg">MySQL</div>
                 <span class="text-sm text-gray-700">Database</span>
               </div>
             </div>
@@ -87,7 +87,7 @@
                 <img src="./icons/kubernetes.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">Kubernetes & Docker</h4>
+                <div class="text-lg">Kubernetes & Docker</div>
                 <span class="text-sm text-gray-700">Devops</span>
               </div>
             </div>
@@ -98,7 +98,7 @@
                 <img src="./icons/git.svg" class="w-8" alt="vuejs">
               </div>
               <div class="flex-none">
-                <h4 class="text-lg">Git & Github</h4>
+                <div class="text-lg">Git & Github</div>
                 <span class="text-sm text-gray-700">Version control</span>
               </div>
             </div>
@@ -111,9 +111,9 @@
 
 <section class="bg-gray-100 py-10">
   <div class="text-center">
-    <h1 class="text-4xl font-medium">Work History</h1>
+    <h1 class="text-3xl font-medium">Experience</h1>
   </div>
-  <div class="container mx-auto max-w-2xl px-4 my-10">
+  <div class="container mx-auto max-w-2xl px-4 my-8">
     <ul>
       <li>
         <span class="text-xs text-gray-800 uppercase">March 2020 - Present</span>
@@ -150,6 +150,86 @@
         <h3 class="text-lg text-gray-800">Studied Computer Science</h3>
       </li>
     </ul>
+  </div>
+</section>
+
+<section class="py-10">
+  <div class="text-center">
+    <h1 class="text-3xl font-medium">Portfolio</h1>
+  </div>
+  <div class="container mx-auto max-w-2xl px-4 my-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="relative shadow-xs p-4">
+        <h3 class="text-lg font-bold pb-2">jamesvan-ca</h3>
+        <p class="text-sm pb-10">
+          A server-side rendered Svelte/Sapper app deployed to Google App Engine. 
+        </p>
+        <p class="absolute bottom-0 w-full text-right pb-4 pr-8">
+          <a href="https://github.com/jvanst/jamesvan-ca" target="_blank" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm py-1 px-3 rounded inline-flex items-center">
+            <span>Source</span>
+            <img src="./icons/open-in-new.svg" class="w-4 ml-2" alt="Open in new tab">
+          </a>
+        </p>
+      </div>
+      <div class="relative shadow-xs p-4">
+        <h3 class="text-lg font-bold pb-2">vue-vuetify-ssr</h3>
+        <p class="text-sm pb-10">
+          A template for building server-side rendered Vue apps with the Vuetify component library.
+        </p>
+        <p class="absolute bottom-0 w-full text-right pb-4 pr-8">
+          <a href="https://github.com/jvanst/vue-vuetify-ssr" target="_blank" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm py-1 px-3 rounded inline-flex items-center">
+            <span>Source</span>
+            <img src="./icons/open-in-new.svg" class="w-4 ml-2" alt="Open in new tab">
+          </a>
+        </p>
+      </div>
+      <div class="relative shadow-xs p-4">
+        <h3 class="text-lg font-bold pb-2">battalion-dedicated-server</h3>
+        <p class="text-sm pb-10">
+          A Battalion1944 linux server installation and update command line tool written in python.
+        </p>
+        <p class="absolute bottom-0 w-full text-right pb-4 pr-8">
+          <a href="https://github.com/jvanst/battalion-dedicated-server" target="_blank" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm py-1 px-3 rounded inline-flex items-center">
+            <span>Source</span>
+            <img src="./icons/open-in-new.svg" class="w-4 ml-2" alt="Open in new tab">
+          </a>
+        </p>
+      </div>
+      <div class="relative shadow-xs p-4">
+        <h3 class="text-lg font-bold pb-2">banksole</h3>
+        <p class="text-sm pb-10">
+          A mock banking console application for Linux.
+        </p>
+        <p class="absolute bottom-0 w-full text-right pb-4 pr-8">
+          <a href="https://github.com/jvanst/banksole" target="_blank" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold text-sm py-1 px-3 rounded inline-flex items-center">
+            <span>Source</span>
+            <img src="./icons/open-in-new.svg" class="w-4 ml-2" alt="Open in new tab">
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="bg-gray-800 py-10">
+  <div class="text-center">
+    <h1 class="text-3xl font-medium text-white">Even more</h1>
+  </div>
+  <div class="container mx-auto max-w-4xl px-4 my-8">
+    <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4 m-auto text-white text-lg">
+      <div class="text-center">
+        <a href="/" class="mt-3"><img src="./icons/post-outline.svg" class="w-6 mr-2 pb-1 inline-block" alt="">Follow my blog</a>
+      </div>
+      <div class="text-center">
+        <a href="https://ca.linkedin.com/in/jamesvansteenkiste"><img src="./icons/linkedin.svg" class="w-6 mr-2 pb-1 inline-block" alt="">Connect with me</a>
+      </div>
+      <div class="text-center">
+        <a href="https://github.com/jvanst"><img src="./icons/github.svg" class="w-6 mr-2 pb-1 inline-block" alt="">Explore my code</a>
+      </div>
+      <div class="text-center">
+        <a href="https://twitter.com/JamesRyanVan"><img src="./icons/twitter.svg" class="w-6 mr-2 pb-1 inline-block" alt="">Tweet at me</a>
+      </div>
+    </div>
   </div>
 </section>
 
