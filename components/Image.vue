3089fb893f835cb4823ca807962852a7afc0348a
James Vansteenkiste <james@jamesvan.ca>
2022-05-29 18:30:24 -0400

    add: image & quote components

---
+++ components/Image.vue
@@ -0,0 +1,33 @@
+<template>
+  <img :src="source" alt="" lazy />
+</template>
+
+<script lang="ts" setup>
+import imageUrlBuilder from "@sanity/image-url";
+
+interface Props {
+  asset: {
+    _ref: string;
+    _type: string;
+  };
+  width?: number;
+}
+
+const { asset, width = 960 } = defineProps<Props>();
+
+const builder = imageUrlBuilder({
+  clientConfig: {
+    projectId: "942rgs6c",
+    dataset: "production",
+  },
+});
+
+// https://www.sanity.io/docs/image-url
+const source = builder
+  .image(asset)
+  .auto("format")
+  .width(width)
+  .fit("crop")
+  .quality(75)
+  .url();
+</script>
